bbad324b2a16d1a0115564431f74511390092b2f
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2017-07-12 16:54:54 -0400

    added post route and completed section

--- app.js
+++ app.js
@@ -16,14 +16,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 
+let todoIdx = 0;
+let context = {
+ todos: [
+   'Wash the car', 'Take out trash', 'Feed the Dog', 'Check Email'
+ ],
+ todoId: function() {
+   return todoIdx++;
+ },
+ completedTodos: ['Made the bed'],
+ completedId: function(){
+   return todoIdx++;
+ }
+};
 
-app.get("/todo", function (req, res) {
+app.get("/", function (req, res) {
+  res.render('index', context);
+});
 
-  var todos = {todos:[
-    'Wash the car', 'Take out trash', 'Feed the Dog','Check Email'
-  ]};
-
-  res.render('index', todos);
+app.post('/', (req, res) => {
+  var todos = context.todos;
+  todos.push(req.body.newTodo);
+  res.redirect('/');
 });
 
 app.listen(3000, function(req, res){
